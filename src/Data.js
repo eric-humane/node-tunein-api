be8f6307adfc4526f050a0709ef4ecfd17af71a3
eric-humane <ericl@humane.com>
2024-10-18 22:39:12 -0400

    use fetch

--- src/Data.js
+++ src/Data.js
@@ -1,14 +1,1 @@
-const { get } = require('https');
-
-exports.get = (url) => {
-    return new Promise((resolve, reject) => {
-        get(url, (r) => {
-            r.setEncoding('utf8');
-
-            let d = '';
-
-            r.on('data', c => d += c);
-            r.on('end', () => resolve(d));
-        }).on('error', reject);
-    });
-}
+exports.get = (url) => fetch(url).then(res => res.text());
